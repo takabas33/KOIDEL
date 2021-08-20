2eb8cf788762c8e598df1012a796fe810ab7933c
takabas33 <88937809+takabas33@users.noreply.github.com>
2021-08-20 17:47:47 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,318 @@
+
+
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>koidel | コイデル</title>
+		<meta name="keywords" content="koidel,コイデル,レンタルサイクルサイト">
+		<meta name="description" content="koidelのオフィシャルサイト">
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+		<meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+		<link rel="icon" href="favicon.ico">
+		<link rel="stylesheet" href="css/style.css">
+        <script src="js/jquery.min.js"></script>
+		<script src="js/pagetop.js"></script>
+        <script src="js/modal.js"></script>
+		<script src="js/scroll.js"></script>
+		<script src="js/scroll_pass.js"></script>
+		<script src="js/jquery.drawsvg.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1BTPVL3c2EsSFKU35bS4np060CD0b4bk&callback=initMap"
+    async defer></script>
+	</head>
+
+	
+	<body>
+		<div class="wrap">
+	 <main> 	
+	     <div class="header_wrap">
+	         <ul class="header_navigation">
+				<li><a href="#1">ABOUT</a></li>
+				<li><a href="#2">GOOGLE MAP</a></li>
+				<li><a href="#3">APPLICASTION</a></li>
+				<li><a href="#4">CONTACT</a></li>
+		     </ul>
+		  </div>
+			
+					
+		        <div class="main_img"></div>	
+		        <div class="main_login">
+	            <div id="orver">
+	            <div id="wrap">
+	            <div id="contents">
+		        <a id="modal-open"  class="fl_tw2">
+                <span>ログイン</span>
+                </a>
+			    </div>
+	            </div> 
+	            </div> 
+	            </div> 
+		        <a href="#1"><div class="main_logo"></div></a>		
+	</main>
+	
+		
+		<container_about>
+		<div id="1"><div class="container_about_h2span"><img src="images/logo18.png" alt="コイデルについて"></div></div>	
+		
+			<container>
+				
+				<div class="container">
+				<div class="container_iti"></div>
+				<div class="container_img"></div>	
+			    <div class="container_article_wrap">
+				<div class="container_no"><h1><span>#</span>01<strong>~ABOUT~</strong></h1></div>
+			    <div class="container_top"><h3><span>"レンタル"</span>できる自転車。</h3></div>
+		        <div class="container_article">
+				<div class="container_article_first"><p>コイデルは、皆様に自転車のある生活を提供します。</p></div>
+				<div class="container_article_second"><p>今まで気軽に行けなかった場所や店。</p></div>
+				<div class="container_article_third"><p>新しい出会いや発見。</p></div>
+				<div class="container_article_third"><p>ひとりで漕ぐのも。好きな人と漕ぐのも。</p></div>
+				<div class="container_article_third"><p>今は買うから借りるの時代。</p></div>
+				</div>
+				</div>
+				</div>
+				<hr>
+			</container>
+			
+		
+			
+			
+			<container2>
+				
+			    <div class="container2">	
+				<div class="container_iti2"></div>
+				<div class="container2_img"></div>
+				<div class="container2_article_wrap">
+				<div class="container_no2"><h1><span>#</span>02<strong>~HOW TO WORKS~</strong></h1></div>
+			    <div class="container2_top"><h3>コイデルは<span>"３サイクル"</span>。</h3></div>
+		        <div class="container2_article">
+				<div class="container2_article_first"><p>まずは新規登録してください。</p></div>
+				<div class="container2_article_second"><p>そしたらアプリで日時と場所を入れて予約する。</p></div>
+				<div class="container2_article_third"><p>アプリを取れば３サイクルを繰り返しです。</p></div>
+				<div class="container2_article_third"><p>漕いだ後は駅のコイデルパーキングに返すだけ。</p></div>
+				<div class="container2_article_third"><p>借りる場所と返す場所は同じじゃなくても大丈夫です。</p></div>
+				</div>
+				</div>
+				</div>
+			    <hr>
+			</container2>
+			
+			<container3>
+				
+			    <div class="container3">
+				<div class="container_iti3"></div>
+				<div class="phone">
+		        <div class="phone__screen">
+		        <div class="phone_image" ><img src="images/phone.png" alt="application" ></div>
+		        </div>
+		        </div>
+				
+				<div class="container3_article_wrap">
+				<div class="container_no3"><h1><span>#</span>03<strong>~HOW TO USE~</strong></h1></div>
+			    <div class="container3_top"><h3>アプリで<span>"即日予約"</span>。</h3></div>
+		        <div class="container3_article">
+				<div class="container3_article_first"><p>あなたが借りたい時、アプリを使えばすぐに借りることができます。</p></div>
+				<div class="container3_article_second"><p>記入するのは日付と借りたい駅と台数のみ。</p></div>
+				<div class="container3_article_third"><p>コイデルは自転車に位置情報をつけてます。</p></div>
+				<div class="container3_article_third"><p>そのため、漕いだ距離など計算してカロリーなども測れます。</p></div>
+				<div class="container3_article_third"><p>漕いだその日は特別にハンバーガーも食べてもよろしいです！</p></div>
+					<div class="registration_logo2">
+			<div class="app2">
+		    <div class="Appstore2"><a href="#"></a></div>
+			<div class="Google_play2"><a href="#"></a></div>
+			</div>
+			</div>
+				</div>
+				</div>
+				</div>
+				
+				
+		<hr>
+			</container3>
+		
+		
+		<container4>
+				
+			    <div class="container4">
+				<div class="container_iti4"></div>
+				<div class="container4_img"></div>
+			    <div class="container4_article_wrap">
+				<div class="container_no4"><h1><span>#</span>04<strong>~SERVICE~</strong></h1></div>
+			    <div class="container4_top"><h3>恋の割引サポート<span>"チャリ恋"</span>。</h3></div>
+		        <div class="container4_article">
+				<div class="container4_article_first"><p>コイデルはカップルを応援します。</p></div>
+				<div class="container4_article_second"><p>もっと恋を初めて欲しい！楽しんで欲しい！</p></div>
+				<div class="container4_article_third"><p>"チャリ恋"カップルで予約すると割引ができるサービスを作りました。</p></div>
+				<div class="container4_article_third"><p>デートのプランにコイデルを活用してみてください。</p></div>
+				<div class="container4_article_third"><p>ハッシュタグ＃チャリ恋で投稿してみよう。</p></div>
+				</div>
+				</div>
+				</div>
+			    <hr>
+			</container4>
+			
+        <container_map>
+	    <div id="2"><div class="container_map_h2span"><img src="images/logo19.png" alt="googlemap"></div></div>
+		</container_map>
+			
+        <div id="map">
+            <script>
+	        var infoWindow=[];
+            var marker=[];
+            var center = {
+            lat: 35.689439, // 緯度
+            lng: 139.70026 // 経度
+            };
+  
+    function initMap() {
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 35.689439, lng: 139.70026},
+        zoom: 14
+      });
+		var contentString = '<div id="content">'+
+          '<h1 id="firstHeading" class="firstHeading">JR新宿駅西口</h1>'+
+          '</div>'+
+          '</div>';
+      var markerData = [ // マーカーを立てる場所名・緯度・経度
+     {
+     name: '新宿御苑',
+     lat: 35.6851763,
+      lng: 139.7100517,
+     },
+　　　{
+     name: 'コクーンタワー',
+     lat: 35.6916628,
+      lng: 139.6949223,
+     }
+     ];
+		
+		   // ここでinfoWindowのスタイリング
+      var iwOuter = $('.gm-style-iw');
+      // prevで直前の兄弟要素を取得
+      var iwBackground = iwOuter.prev();
+      // nextで直後の兄弟要素を取得
+      var iwCloseBtn = iwOuter.next();
+      // 背景のshadowを消す
+      iwBackground.children(':nth-child(2)').css("display", "none");
+      // 白い背景を消す
+      iwBackground.children(':nth-child(4)').css("display", "none");
+      // 下のとんがりコーンの影を消す
+      iwBackground.children(':nth-child(1)').css("display", "none");
+      // なんかまだとんがりコーンの影がしつこく出てくるからここで消す(左の影)
+      iwBackground.children(':nth-child(3)').children(':nth-child(1)').children(':nth-child(1)').css("box-shadow", "none");
+      // なんかまだとんがりコーンの影がしつこく出てくるからここで消す(右の影)
+      iwBackground.children(':nth-child(3)').children(':nth-child(2)').children(':nth-child(1)').css("box-shadow", "none");
+      // ここでcssいじるのめんどいからクラスをつける
+      iwCloseBtn.addClass("closebtn");
+      // fontawesomeのcloseボタンを追加
+      iwCloseBtn.prepend('<i class="fa fa-times-circle" aria-hidden="true"></i>');
+		for (var i = 0; i < markerData.length; i++) {
+        markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
+        marker[i] = new google.maps.Marker({ // マーカーの追加
+         position: markerLatLng, // マーカーを立てる位置を指定
+            map: map // マーカーを立てる地図を指定
+       });}
+       marker = new google.maps.Marker({ // マーカーの追加
+       position: center, // マーカーを立てる位置を指定
+       map: map // マーカーを立てる地図を指定
+       });
+       infoWindow = new google.maps.InfoWindow({ // 吹き出しの追加
+       content: contentString // 吹き出しに表示する内容
+       });
+       marker.addListener('click', function(i) { // マーカーをクリックしたとき
+       infoWindow.open(map, marker); // 吹き出しの表示
+       });
+       }
+       </script>
+		
+       <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1BTPVL3c2EsSFKU35bS4np060CD0b4bk&callback=initMap"async defer></script>
+		
+	   </div>
+			
+			
+	　　　　　<container_app>
+			 <div id="3"><div class="container_app_h2span"><img src="images/logo16.png" alt="application"></div></div>
+            </container_app>
+			
+			<div class="registration">
+		    <div class="registration_wrap">
+		    <div class="registration_article"></div>
+			<div class="phone2"> </div>
+			</div>
+			
+		    <div class="registration_logo">
+			<div class="registation_bicycle"><img src="images/logo12.png" alt="コイデル"></div>
+			<div class="app">
+		    <div class="Appstore"><a href="#"></a></div>
+			<div class="Google_play"><a href="#"></a></div>
+			</div>
+			</div>
+		    </div>
+		    
+		    <container_contact>
+			<div id="4"><div class="container_contact_h2span"><img src="images/logo17.png" alt="コンタクト"></div></div>
+            </container_contact>
+		
+
+
+		    </div>
+			</div>
+			</div>
+		
+<iframe width="100%" height="1000" src="//select-type.com/e/?id=AD3LEWtAaMM&w_flg=1" frameborder="0"></iframe>
+			
+			
+			
+			
+		<fotter>
+		      <div class="footer_logo"></div>
+			  <div class="footer_wrap"> 
+			  <p>Copy Right 2018 Takahito Onishi</p>
+			  </div>
+	</fotter>
+			</div>
+	
+	
+		
+	<modal>
+	         <div id="modal-content">
+	         <div id="modal-content-innar">
+	         <div class="login">
+		     <p><a id="modal-close" class="button-link fa-2x"></a></p>
+             <div class="heading">
+			 <p><a2 id="modal-close" class="button-link fa-2x"></a2></p>
+             <a></a>
+				
+			  <form class="form_login" action="reservasion.php" method="POST" >
+			  <div class="input_block login_input">
+			  <div class="input-group input-group-lg">
+		      <span class="input-group-addon fa-2x"><i class="fa fa-user "></i></span>
+			  <div><input type="text" class="form-control_name" name="username" size="20"  placeholder="Username or email"></div>
+	   	      </div>
+		      <div class="input-group input-group-lg">
+              <span class="input-group-addon"><i class="fa fa-lock fa-2x"></i></span>
+              <div class="input_block login_input">
+		      <div><input type="passward" class="form-control_pass" name="pass"  placeholder="Password"></div>
+			  </div>
+			  <button type="submit"  name="login" value="ログイン" class="float" >ログイン</button>
+			  </div>
+              </div> 
+			  </form>
+		      </div>
+	          </div>
+			  </div>
+			  </div>
+	</modal> 
+	
+
+		
+
+	
+		
+</body>
+</html>
